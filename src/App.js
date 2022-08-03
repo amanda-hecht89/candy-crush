1f8ae830660be93c159b4a3f9815f09fbe002c78
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-02 20:17:17 -0600

    3 and 4 disappear

--- src/App.js
+++ src/App.js
@@ -32,6 +32,43 @@ function App() {
   };
 
 
+  const checkColFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const decidedColor = currentColor[i];
+      if (columnOfFour.every(square => currentColor[square] === decidedColor)) {
+        columnOfFour.forEach(square => currentColor[square] = '');
+      }
+    }
+  };
+
+
+  const checkRowThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2];
+      const decidedColor = currentColor[i];
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64];
+      if (notValid.includes(i)) continue;
+      if (rowOfThree.every(square => currentColor[square] === decidedColor)) {
+        rowOfThree.forEach(square => currentColor[square] = '');
+      }
+    }
+  };
+
+
+  const checkRowFour = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3];
+      const decidedColor = currentColor[i];
+      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64];
+      if (notValid.includes(i)) continue;
+      if (rowOfFour.every(square => currentColor[square] === decidedColor)) {
+        rowOfFour.forEach(square => currentColor[square] = '');
+      }
+    }
+  };
+
+
   const createBoard = () => {
     const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
@@ -51,12 +88,15 @@ function App() {
 
   useEffect(() => {
     const timer = setInterval(() => {
+      checkColFour();
+      checkRowFour();
       checkColThree();
+      checkRowThree();
       setCurrentColor([...currentColor]);
 
     }, 100);
     return () => clearInterval(timer);
-  }, [checkColThree, currentColor]);
+  }, [checkColFour, checkRowFour, checkColThree, checkRowThree, currentColor]);
 
 
 
